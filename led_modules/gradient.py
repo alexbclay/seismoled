4d579d4031c5044836ce779a3eb72cd52bf43665
Alex Clay <alexbclay@gmail.com>
2016-01-07 22:31:37 -0500

    Test runs a basic gradient!

--- led_modules/gradient.py
+++ led_modules/gradient.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from module_base import LedActionModule
 import random
+from math import ceil, floor
 
 class Gradient(LedActionModule):
     def __init__(self, length, color_start=None, color_end=None):
         ''' Set the colors for the gradient '''
-#        import pdb; pdb.set_trace()
-#        super(Gradient, self).__init__(length)
+        #        import pdb; pdb.set_trace()
+        #        super(Gradient, self).__init__(length)
 
+        self.length = length
         if color_start:
             self.color_start = color_start
         else:
@@ -29,14 +31,25 @@ class Gradient(LedActionModule):
                           (255 - min[i])))
                      for i in range(3))
 
-    def trigger(self):
-        return self.make_gradient
+    def time_step(self):
+        self.color_start = self.random_color()
+        self.color_end = self.random_color()
+        return self.make_gradient()
 
-    def make_gradient(self, length=None, color_start=[], color_end=[]):
+    def trigger(self):
+        return self.make_gradient()
+
+    def make_gradient(self, length=None, color_start=None, color_end=None):
+
         if not length:
             length = self.length
+        if not color_start:
+            color_start = self.color_start
+        if not color_end:
+            color_end = self.color_end
+
         diff = map(lambda x,y: (y - x) / (float(length) - 1), color_start, color_end)
         result = []
         for i in range(length):
-            result.append(map(lambda x,y: int(ceil(x + y * i)), color_start, diff))
+            result.append(tuple(map(lambda x,y: int(ceil(x + y * i)), color_start, diff)))
         return result
--- seismo_test.py
+++ seismo_test.py
@@ -9,29 +9,63 @@ if __name__ == '__main__':
 '''
 
 import Tkinter as tk
-
+import random
+import led_modules
 
 class App(tk.Frame):
-    def __init__(self, master=None):
+    num_leds = 24
+    module = led_modules.gradient.Gradient(num_leds)
+    def __init__(self, master=None, len=24):
         tk.Frame.__init__(self, master)
 
         self.grid()
         self.createWidgets()
 
     def createWidgets(self):
-        def make_fun(i):
-            def p ():
-                print "button {}".format(i)
-            return p
+        # make a canvas to draw on
+        self.led_canvas = tk.Canvas(self, width=100, height=24*40)
+        self.led_canvas.grid()
 
-        for i in range(10):
-            b = tk.Button(self, text='Button {}'.format(i), command=make_fun(i))
-            b.grid()
+        self.step()
 
+        # button for steps
+        self.stepButton = tk.Button(self, text='Step', command=self.step)
+        self.stepButton.grid()
+
+        # button to quit~
         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
         self.quitButton.grid()
 
+    def setup(length, module):
+        self.num_leds=24
+        self.module = module
+
+    def do_time_step(self):
+        self.draw_array(self.module.time_step())
+        self.after(200, self.do_time_step)
+
+    def draw_array(self, array):
+        if not array:
+            return
+        for i in range(min(24,len(array))):
+            self.led_canvas.create_rectangle(6,5 + 40*i+1, 98, 40*(i+1)-1, fill='#%02x%02x%02x' % array[i])
+        for i in range(self.num_leds):
+            self.led_canvas.create_rectangle(5,5 + 40*i, 99, 40*(i+1),)
+        print array
+
+    def step(self):
+        self.draw_array(self.module.trigger())
+        '''
+        start_color = tuple(random.random() * 255 for i in range(3))
+        inc_color = tuple(random.random() * 25 for i in range(3))
+
+        print start_color
+        print inc_color
+        self.draw_array([tuple((start_color[i] + (inc_color[i] * led)) % 255 for i in range(3)) for led in range(self.num_leds)])
+        '''
+
 app = App()
 
-app.master.title('Sample')
+app.master.title('Stairs Simulator!')
+app.after(200,app.do_time_step)
 app.mainloop()
